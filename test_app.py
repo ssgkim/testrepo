f0d814c51d7975600a7d125fc5a6190a4ad6e76a
Trainersudhanshu <124223047+Trainersudhanshu@users.noreply.github.com>
2024-01-09 23:13:06 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = quote('Hello, World! This is a simple Flask app 12333.')
+    expected_text = quote('Hello')
     assert expected_text.encode() in response.data
