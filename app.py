79e4e4442ef5c40b571e9554d0b9a0fe9ef3d7fd
Trainersudhanshu <124223047+Trainersudhanshu@users.noreply.github.com>
2024-01-26 19:38:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask import Flask
 from urllib.parse import quote 
 
 def create_app():
+    x=10
+    y=100
+    z=2000000000
     app = Flask(__name__)
 
     @app.route('/')
@@ -11,6 +14,9 @@ def create_app():
         return 'gfg'
 
     return app
+
+def a():
+    a()
 
 if __name__ == '__main__':
     app = create_app()
